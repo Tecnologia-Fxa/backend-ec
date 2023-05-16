d01a584653dbd24be25aa8b0feefb422311b8e89
Tecnologia FXA <sistemas@fuxiaaccesorios.com>
2023-05-16 10:26:39 -0500

    Validacion del secreto de wp

--- middlewares/ValidateSecretWP.js
+++ middlewares/ValidateSecretWP.js
@@ -1,12 +1,21 @@
+const crypto = require('crypto');
+
 require('dotenv').config(); // Cargar las variables de entorno
 
 const ValidateSecretWp = (req,res,next)=>{
 
     const signature = req.headers['x-wc-webhook-signature'];
-
+    const payload = JSON.stringify(req.body);
+    
+    const generatedSignature = crypto
+    .createHmac('sha256', )
+    .update(payload)
+    .digest('base64');
+    
     console.log(signature)
-
-    if (process.env.SECRET === signature) {
+    console.log(generatedSignature)
+  
+    if (generatedSignature === signature) {
       // La firma es válida, la solicitud es auténtica
       // Procesa la solicitud del webhook aquí
       console.log('Solicitud de webhook válida');
