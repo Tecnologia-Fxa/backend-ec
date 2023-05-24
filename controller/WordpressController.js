4ff481b8e633598b3215cbdfd703ef66c793e77a
Tecnologia FXA <sistemas@fuxiaaccesorios.com>
2023-05-24 13:57:08 -0500

    Con estado cotizador

--- controller/WordpressController.js
+++ controller/WordpressController.js
@@ -27,10 +27,10 @@ const WordpressController = {
             })
     
             // Enviar una respuesta al servidor de WooCommerce
-            res.json({costo_envio:ciudad.trayecto_envio.costo_trayecto_envio});
+            res.json({costo_envio:ciudad.trayecto_envio.costo_trayecto_envio, estado_cotizador:1});
             
         }else
-            res.json({costo_envio:1000});
+            res.json({costo_envio:10, estado_cotizador:3});
     }
 
 }
