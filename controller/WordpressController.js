bb2a795dc55df1d4adbeb27056f3d6b27f49ca47
Tecnologia FXA <sistemas@fuxiaaccesorios.com>
2023-05-24 12:49:08 -0500

    Se crea funcion de cotizador de envios ahora con valdiacion de campos basica

--- controller/WordpressController.js
+++ controller/WordpressController.js
@@ -9,23 +9,28 @@ const WordpressController = {
         //mostrar resultado del webhook
         console.log('Solicitud de webhook recibida:', body);
 
-        let nomenclatura_provincia = body.provincia.split(".")[1]
+        if(body.provincia && body.provincia !== '' && body.poblacion && body.poblacion !== ''){
     
-        const estado = await ProvinciaModel.findOne({where:{nomenclatura_provincia}, attributes:['id_provincia']})
+            let nomenclatura_provincia = body.provincia.split(".")[1]
+        
+            const estado = await ProvinciaModel.findOne({where:{nomenclatura_provincia}, attributes:['id_provincia']})
+        
+            const ciudad = await CiudadModel.findOne({where:{
+                nombre_ciudad:body.poblacion,
+                cod_provincia_fk:estado.id_provincia
+                },
+                attributes:['id_ciudad'],
+                include:{
+                    model:TrayectoEnvioModel,
+                    attributes:["costo_trayecto_envio"]
+                }
+            })
     
-        const ciudad = await CiudadModel.findOne({where:{
-            nombre_ciudad:body.poblacion,
-            cod_provincia_fk:estado.id_provincia
-            },
-            attributes:['id_ciudad'],
-            include:{
-                model:TrayectoEnvioModel,
-                attributes:["costo_trayecto_envio"]
-            }
-        })
-
-        // Enviar una respuesta al servidor de WooCommerce
-        res.json({costo_envio:ciudad.trayecto_envio.costo_trayecto_envio});
+            // Enviar una respuesta al servidor de WooCommerce
+            res.json({costo_envio:ciudad.trayecto_envio.costo_trayecto_envio});
+            
+        }
+        res.json({costo_envio:1000});
     }
 
 }
